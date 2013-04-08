9d75160879991da40d7152650dcccd724ae3851f
Nico Schottelius <nico@bento.schottelius.org>
2013-04-08 16:09:20 +0200

    import cconfig from nico.schotteli.us
    Signed-off-by: Nico Schottelius <nico@bento.schottelius.org>

---
+++ docs/cconfig/cconfig/labels.pl
@@ -0,0 +1,69 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_bzip2/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cinit/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_xml/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/requirements/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/port-links/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_posix/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_utf8/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/port-dir/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_samba/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mini-lpd/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_tar/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/port-dir/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/requirements/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/port-links/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+1;
+
